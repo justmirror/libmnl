justmirror/libmnl

commit a5f25889216411ad7492047fafe6de03b8408440

relax mnl_attr_type_valid() checkings and change errno value
This patch relaxes strict attribute checkings in the example files.
I have also changed the errno value, now it's EOPNOTSUPP instead of
EINVAL.

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

examples/rtnl-link-dump3.c
@@ -31,10 +31,10 @@ static int data_cb(const struct nlmsghdr *nlh, void *data)
 	mnl_attr_for_each(attr, nlh, sizeof(*ifm)) {
 		int type = mnl_attr_get_type(attr);
 
-		if (mnl_attr_type_valid(attr, IFLA_MAX) < 0) {
-			perror("mnl_attr_type_valid");
-			return MNL_CB_ERROR;
-		}
+		/* skip unsupported attribute in user-space */
+		if (mnl_attr_type_valid(attr, IFLA_MAX) < 0)
+			return MNL_CB_OK;
+
 		switch(type) {
 		case IFLA_MTU:
 			if (mnl_attr_validate(attr, MNL_TYPE_U32) < 0) {

examples/genl-family-get.c
@@ -17,10 +17,9 @@ static int parse_mc_grps_cb(const struct nlattr *attr, void *data)
 	const struct nlattr **tb = (const struct nlattr **)data;
 	int type = mnl_attr_get_type(attr);
 
-	if (mnl_attr_type_valid(attr, CTRL_ATTR_MCAST_GRP_MAX) < 0) {
-		perror("mnl_attr_type_valid");
-		return MNL_CB_ERROR;
-	}
+	/* skip unsupported attribute in user-space */
+	if (mnl_attr_type_valid(attr, CTRL_ATTR_MCAST_GRP_MAX) < 0)
+		return MNL_CB_OK;
 
 	switch(type) {
 	case CTRL_ATTR_MCAST_GRP_ID:
@@ -65,10 +64,8 @@ static int parse_family_ops_cb(const struct nlattr *attr, void *data)
 	const struct nlattr **tb = (const struct nlattr **)data;
 	int type = mnl_attr_get_type(attr);
 
-	if (mnl_attr_type_valid(attr, CTRL_ATTR_OP_MAX) < 0) {
-		perror("mnl_attr_type_valid");
-		return MNL_CB_ERROR;
-	}
+	if (mnl_attr_type_valid(attr, CTRL_ATTR_OP_MAX) < 0)
+		return MNL_CB_OK;
 
 	switch(type) {
 	case CTRL_ATTR_OP_ID:
@@ -110,10 +107,8 @@ static int data_attr_cb(const struct nlattr *attr, void *data)
 	const struct nlattr **tb = (const struct nlattr **)data;
 	int type = mnl_attr_get_type(attr);
 
-	if (mnl_attr_type_valid(attr, CTRL_ATTR_MAX) < 0) {
-		perror("mnl_attr_type_valid");
-		return MNL_CB_ERROR;
-	}
+	if (mnl_attr_type_valid(attr, CTRL_ATTR_MAX) < 0)
+		return MNL_CB_OK;
 
 	switch(type) {
 	case CTRL_ATTR_FAMILY_NAME:

examples/nf-queue.c
@@ -26,10 +26,10 @@ static int parse_attr_cb(const struct nlattr *attr, void *data)
         const struct nlattr **tb = (const struct nlattr **)data;
 	int type = mnl_attr_get_type(attr);
 
-	if (mnl_attr_type_valid(attr, NFQA_MAX) < 0) {
-		perror("mnl_attr_type_valid");
-		return MNL_CB_ERROR;
-	}
+	/* skip unsupported attribute in user-space */
+	if (mnl_attr_type_valid(attr, NFQA_MAX) < 0)
+		return MNL_CB_OK;
+
 	switch(type) {
 	case NFQA_MARK:
 	case NFQA_IFINDEX_INDEV:

examples/rtnl-link-dump.c
@@ -19,10 +19,9 @@ static int data_attr_cb(const struct nlattr *attr, void *data)
 	const struct nlattr **tb = (const struct nlattr **)data;
 	int type = mnl_attr_get_type(attr);
 
-	if (mnl_attr_type_valid(attr, IFLA_MAX) < 0) {
-		perror("mnl_attr_type_valid");
-		return MNL_CB_ERROR;
-	}
+	/* skip unsupported attribute in user-space */
+	if (mnl_attr_type_valid(attr, IFLA_MAX) < 0)
+		return MNL_CB_OK;
 
 	switch(type) {
 	case IFLA_MTU:

examples/rtnl-link-dump2.c
@@ -16,10 +16,9 @@
 
 static int data_attr_cb(const struct nlattr *attr, void *data)
 {
-	if (mnl_attr_type_valid(attr, IFLA_MAX) < 0) {
-		perror("mnl_attr_type_valid");
-		return MNL_CB_ERROR;
-	}
+	/* skip unsupported attribute in user-space */
+	if (mnl_attr_type_valid(attr, IFLA_MAX) < 0)
+		return MNL_CB_OK;
 
 	switch(mnl_attr_get_type(attr)) {
 	case IFLA_MTU:

examples/rtnl-link-event.c
@@ -18,10 +18,9 @@ static int data_attr_cb(const struct nlattr *attr, void *data)
 	const struct nlattr **tb = (const struct nlattr **)data;
 	int type = mnl_attr_get_type(attr);
 
-	if (mnl_attr_type_valid(attr, IFLA_MAX) < 0) {
-		perror("mnl_attr_type_valid");
-		return MNL_CB_ERROR;
-	}
+	/* skip unsupported attribute in user-space */
+	if (mnl_attr_type_valid(attr, IFLA_MAX) < 0)
+		return MNL_CB_OK;
 
 	switch(type) {
 	case IFLA_MTU:

examples/rtnl-route-dump.c
@@ -17,10 +17,10 @@
 
 static int data_attr_cb2(const struct nlattr *attr, void *data)
 {
-	if (mnl_attr_type_valid(attr, RTAX_MAX) < 0) {
-		perror("mnl_attr_type_valid");
-		return MNL_CB_ERROR;
-	}
+	/* skip unsupported attribute in user-space */
+	if (mnl_attr_type_valid(attr, RTAX_MAX) < 0)
+		return MNL_CB_OK;
+
 	if (mnl_attr_validate(attr, MNL_TYPE_U32) < 0) {
 		perror("mnl_attr_validate");
 		return MNL_CB_ERROR;
@@ -76,10 +76,9 @@ static int data_attr_cb(const struct nlattr *attr, void *data)
 	const struct nlattr **tb = (const struct nlattr **)data;
 	int type = mnl_attr_get_type(attr);
 
-	if (mnl_attr_type_valid(attr, RTA_MAX) < 0) {
-		perror("mnl_attr_type_valid");
-		return MNL_CB_ERROR;
-	}
+	/* skip unsupported attribute in user-space */
+	if (mnl_attr_type_valid(attr, RTA_MAX) < 0)
+		return MNL_CB_OK;
 
 	switch(type) {
 	case RTA_TABLE:

src/attr.c
@@ -115,11 +115,16 @@ struct nlattr *mnl_attr_next(const struct nlattr *attr, int *len)
  * This function allows to check if the attribute type is higher than the
  * maximum supported type. If the attribute type is invalid, this function
  * returns -1 and errno is explicitly set. On success, this function returns 1.
+ *
+ * Strict attribute checking in user-space is not a good idea since you may
+ * run an old application with a newer kernel that supports new attributes.
+ * This leads to backward compatibility breakages in user-space. Better check
+ * if you support an attribute, if not, skip it.
  */
 int mnl_attr_type_valid(const struct nlattr *attr, uint16_t max)
 {
 	if (mnl_attr_get_type(attr) > max) {
-		errno = EINVAL;
+		errno = EOPNOTSUPP;
 		return -1;
 	}
 	return 1;

commit 4c811881d33c702464e7bf02e8ce16b3cc3eb1fc

fix rtnl-link-dump3.c
commit a5f25889216411ad7492047fafe6de03b8408440 introduced a minor
problem in this example.

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

examples/rtnl-link-dump3.c
@@ -33,7 +33,7 @@ static int data_cb(const struct nlmsghdr *nlh, void *data)
 
 		/* skip unsupported attribute in user-space */
 		if (mnl_attr_type_valid(attr, IFLA_MAX) < 0)
-			return MNL_CB_OK;
+			continue;
 
 		switch(type) {
 		case IFLA_MTU: