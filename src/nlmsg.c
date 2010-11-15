a2975d2eb058d187db616b67a5e7d88382dbcc6a
Pablo Neira Ayuso <pablo@netfilter.org>
2010-11-15 20:58:38 +0100

    license: change licensing terms from GPLv2+ to LGPLv2.1+
    Existing contributors ACK'ed the license change via email:
    
    * Jozsef Kadlecsik
    * Jan Engelhardt
    * Cristian Rodríguez
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- src/nlmsg.c
+++ src/nlmsg.c
@@ -2,8 +2,8 @@
  * (C) 2008-2010 by Pablo Neira Ayuso <pablo@netfilter.org>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  */
 #include <stdbool.h>
